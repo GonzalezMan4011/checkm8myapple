134fedee36c31394f70e68be3d3bc12540a4b666
Cesar Gonzalez <cesargon4011@gmail.com>
2023-02-03 23:00:21 -0800

    Starting base project (just using templates)

---
+++ checkm8myapple/checkm8myappleApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkm8myappleApp.swift
+//  checkm8myapple
+//
+//  Created by Cesar F. Gonzalez on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct checkm8myappleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
